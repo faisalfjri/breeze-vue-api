6383d889cc157a22378457896b7df2d03fb517d2
Anggit Utomo <pucukpisang97@gmail.com>
2025-07-03 12:32:26 +0700

    feat: :arrow_up: upgrade tailwindcss to v4

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -46,7 +46,7 @@ const auth = store.authUser
             </div>
 
             <div
-                class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="p-6">
                         <div class="flex items-center">
